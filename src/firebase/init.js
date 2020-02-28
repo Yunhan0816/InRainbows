700d08b2198c71a2f0b2933e800820ce10ef9de5
Hannah Huang <yunhanh@bu.edu>
2020-02-28 17:45:31 -0500

    add firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,32 @@
+import firebase from "firebase/app";
+const firebaseui = require("firebaseui");
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyX1-eSGM-myHydqEWc-lmNFIeyQTDCrA",
+  authDomain: "inrainbows-171a7.firebaseapp.com",
+  databaseURL: "https://inrainbows-171a7.firebaseio.com",
+  projectId: "inrainbows-171a7",
+  storageBucket: "inrainbows-171a7.appspot.com",
+  messagingSenderId: "210440959778",
+  appId: "1:210440959778:web:427881d0a0db267fce577b",
+  measurementId: "G-R0KY65S2W4"
+};
+
+const app = firebase.initializeApp(config);
+const auth = firebase.auth();
+const db = app.firestore();
+
+const authUI = new firebaseui.auth.AuthUI(auth);
+
+const authUIConfig = {
+  signInSuccessUrl: "/",
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    }
+  ]
+};
+
+export { auth, db, authUI, authUIConfig };
