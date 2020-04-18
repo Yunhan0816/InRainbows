5be79211c1eabdfef74cab56b80099db328e8d49
Zhenghui <zwan1312@bu.edu>
2020-04-18 19:58:49 -0400

    actual address on gmap

--- src/components/gmap.vue
+++ src/components/gmap.vue
@@ -7,9 +7,28 @@
 
 <script>
 import gmapsInit from "../gmaps";
+import { db } from "@/main";
 export default {
   name: "gmap",
+  data() {
+    return {
+      therapist: []
+    }
+  },
+  methods: {
+    parseAddress(arr) {
+      return arr.street + " " + arr.unit + " " + arr.city + " " + arr.state + " " + arr.zipcode
+    }
+  },
   async mounted() {
+    let snapshot = await db.collection("therapists").get();
+    let therapist = [];
+    snapshot.forEach(doc => {
+      let appData = doc.data();
+      appData.id = doc.id;
+      therapist.push(appData);
+    });
+    this.therapist = therapist;
     try {
       const google = await gmapsInit();
       const geocoder = new google.maps.Geocoder();
@@ -44,49 +63,52 @@ export default {
       //   "TRY THESE COORDS:",
       //   geocodeAddress(geocoder, map, "1079 Commonwealth Ave., Boston MA 02215")
       // );
-      const location = [
-        { lat: 42.3348, lng: -71.0733 },
-        { lat: 42.362, lng: -71.06 },
-        { lat: 42.3361, lng: -71.1075 },
-        { lat: 42.3397, lng: -71.1049 }
-      ];
-
-      geocoder.geocode(
-        { address: "1079 Commonwealth Ave., Boston MA 02215" },
-        function(results, status) {
-          if (status === "OK") {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location
-            });
-            marker.location;
-          } else {
-            alert("shits messed up");
-          }
+      // const location = [
+      //   { lat: 42.3348, lng: -71.0733 },
+      //   { lat: 42.362, lng: -71.06 },
+      //   { lat: 42.3361, lng: -71.1075 },
+      //   { lat: 42.3397, lng: -71.1049 }
+      // ];
+      for (let p in this.therapist){
+        if (p < 3){
+          geocoder.geocode(
+            { address: this.parseAddress(this.therapist[p].address) },
+            function(results, status) {
+              if (status === "OK") {
+                map.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location
+                });
+                marker.location;
+              } else {
+                console.log(p + " no work");
+              }
+            }
+          );
         }
-      );
+      }
 
-      const marker1 = new google.maps.Marker({
-        position: location[0],
-        map: map
-      });
-      marker1.location;
-      const marker2 = new google.maps.Marker({
-        position: location[1],
-        map: map
-      });
-      marker2.location;
-      const marker3 = new google.maps.Marker({
-        position: location[2],
-        map: map
-      });
-      marker3.location;
-      const marker4 = new google.maps.Marker({
-        position: location[3],
-        map: map
-      });
-      marker4.location;
+      // const marker1 = new google.maps.Marker({
+      //   position: location[0],
+      //   map: map
+      // });
+      // marker1.location;
+      // const marker2 = new google.maps.Marker({
+      //   position: location[1],
+      //   map: map
+      // });
+      // marker2.location;
+      // const marker3 = new google.maps.Marker({
+      //   position: location[2],
+      //   map: map
+      // });
+      // marker3.location;
+      // const marker4 = new google.maps.Marker({
+      //   position: location[3],
+      //   map: map
+      // });
+      // marker4.location;
       //   console.log(marker)
     } catch (error) {
       console.error(error);
